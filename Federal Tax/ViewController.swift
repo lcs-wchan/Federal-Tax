0e0b9956a377c949c615aa83d95c16af48c47f92
WilliamChan <wchan@lcs.on.ca>
2019-11-02 10:06:26 -0400

    finished switch statement

--- Federal Tax/ViewController.swift
+++ Federal Tax/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // runs as soon as the view becomes visible
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,3 +19,19 @@ class ViewController: UIViewController {
 
 }
 
+//MARK: Methods
+let name = a
+let grossIncome = 10_000
+
+switch grossIncome{
+case 0...47_630:
+    print("name, your tax owing is grossIncome*0.15")
+case 47_631...95_259:
+    print("name, your tax owing is grossIncome*0.205")
+case 95_260...147_667:
+    print("name, your tax owing is grossIncome*0.26")
+case 147_668...270_371:
+    print("name, your tax owing is grossIncome*0.29")
+default:
+    print("name, your tax owing is grossIncome*0.33")
+}
