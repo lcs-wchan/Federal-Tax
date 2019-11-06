9b76b67673034caab8033bba1c29953980fbb105
WilliamChan <wchan@lcs.on.ca>
2019-11-06 14:34:55 -0500

    finished guard statement

--- Federal Tax/ViewController.swift
+++ Federal Tax/ViewController.swift
@@ -13,26 +13,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var whatIsYourGrossIncome: UITextField!
     // runs as soon as the view becomes visible
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func calculateTax(_ sender: Any) {
-        let name = whatIsYourName.text!
+        guard let name = whatIsYourName.text else{
+            whatIsYourName.text = "Please enter your name"
+        }
         
         let inputedGrossincome = whatIsYourGrossIncome.text!
-        let grossIncome = Double(inputedGrossincome)!
+        guard let grossIncome = Double(inputedGrossincome) else{
+            whatIsYourGrossIncome.text = "Please enter a numeric gross income"
+        }
         
         
         //MARK: Methods
         switch grossIncome {
         case 0...47_630:
             var taxOwing = grossIncome*0.15
-            print("name, your tax owing is taxOwing")
+            print("name, your tax owing is $\(taxOwing)")
         case 47_631...95_259:
             var taxOwing = (grossIncome-47_630)*0.205+47_630*0.15
-            print("name, your tax owing is $taxOwing")
+            print("name, your tax owing is $\(taxOwing)")
         case 95_260...147_667:
             var taxOwing = (grossIncome-95_260)*0.26+47_630*(0.205+0.15)
             print("name, your tax owing is $taxOwing)")
@@ -43,9 +49,12 @@ class ViewController: UIViewController {
             var taxOwing = (grossIncome-270_371)*0.33+122_703*0.29+52_408*0.26+47_630*(0.205+0.15)
             print("name, your tax owing is $taxOwing")
         }
-      let effectiveTaxRate = taxOwing/grossIncome*100
+      let effectiveTaxRate = (taxOwing/grossIncome)*100
         print("Effective tax rate is: effectiveTaxRate% ")
-    }
+        
+        outputtaxOwing.text =
+        
+    
     
     
     
